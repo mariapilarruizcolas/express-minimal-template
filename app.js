ea885f93231f34744edea3a7efe2cd347773016b
Pierre <pierre.genthon@wildcodeschool.fr>
2020-12-06 23:22:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('sending hello world');
-  res.send('Hello world');
+app.get('/myroute', (req, res) => {
+  console.log('handling /myroute');
+  res.send('content for /myroute');
 });
 
 app.listen(5000, () => console.log('server listening on port 5000'));
